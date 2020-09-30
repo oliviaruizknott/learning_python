68d986fcbb4180cbca4f294dedbf359d3b2939e6
Olivia Ruiz-Knott <oruiz-knott@edx.org>
2020-09-29 23:36:50 -0400

    Car game!

---
+++ car_game/car_game.py
@@ -0,0 +1,106 @@
+import pygame, sys
+from pygame.locals import *
+import random, time
+
+pygame.init()
+FramePerSec = pygame.time.Clock()
+
+RED   = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+
+background = pygame.image.load("Street.png")
+
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Car Game")
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Enemy.png")
+        self.surf = pygame.Surface((42,70))
+        self.rect = self.surf.get_rect(center = (random.randint(40,SCREEN_WIDTH-40), 0))
+
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0,SPEED)
+        if (self.rect.top > SCREEN_HEIGHT):
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.surf = pygame.Surface((40, 75))
+        self.rect = self.surf.get_rect(center = (160, 520))
+
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+
+P1 = Player()
+E1 = Enemy()
+
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    DISPLAYSURF.blit(background, (0,0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURF.blit(scores, (10,10))
+
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('crash.wav').play()
+        time.sleep(0.5)
+
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+
+        for entity in all_sprites:
+            entity.kill()
+
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+
+    pygame.display.update()
+    FramePerSec.tick(60)
